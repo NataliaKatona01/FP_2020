8762d10f2aaef716a87bcbde8a32dc90123b7e4c
Natalia Katona <katonanatalia6@gmail.com>
2020-12-29 16:23:45 +0200

    ..

--- Probleme_cu_tablouri/Prob15.cs
+++ Probleme_cu_tablouri/Prob15.cs
@@ -8,5 +8,20 @@ namespace Probleme_cu_tablouri
 {
     class Prob15
     {
+        //Modificati un vector prin eliminarea elementelor care se repeta, fara a folosi un alt vector. 
+
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[100];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = rnd.Next(100);
+
+            }
+        }
     }
 }
